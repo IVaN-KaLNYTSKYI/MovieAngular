d546a82c36617935b3c02d1cf0067cfe44eda3a8
IVaN-KaLNYTSKYI <ivan.kalnytskyi@gmail.com>
2021-06-01 16:28:34 +0300

    done css movie-detail start video movie

--- src/app/components/movie-detail/movie-detail.component.ts
+++ src/app/components/movie-detail/movie-detail.component.ts
@@ -12,9 +12,11 @@ export class MovieDetailComponent implements OnInit {
   movie: ResultsMovie
   private lik = JSON.parse(<string>localStorage.getItem("lik"))
   list = this.lik || []
-
+  movieImg = 'https://image.tmdb.org/t/p/original'
+  id:number
   constructor(private movieServices: MovieService, private activatedRoute: ActivatedRoute) {
     this.activatedRoute.params.subscribe(value => {
+      this.id= +value.id
       this.movieServices.getMovieId(value.id).subscribe(value => {
         this.movie = value
       })
@@ -23,8 +25,24 @@ export class MovieDetailComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  add(poster_path: string) {
-    this.list.push({poster_path})
+
+  per(){
+    return this.list.find((value:any)=>value.id===this.id)
+  }
+  addLike(movie: object) {
+    console.log(this.per())
+    if(!this.per()){
+      this.list.push(movie)
+    }
+    else {
+      alert("no")
+    }
     localStorage.setItem("lik", JSON.stringify(this.list))
+  }
+  stl(url:string){
+    return(
+    `background-image:linear-gradient(rgba(0,0,0,0.9),
+     rgba(0,0,0,0.6)),
+     url(https://image.tmdb.org/t/p/original${url})`)
   }
 }
